8464d839d28f17617f431c9b1e23a75c74644ae4
Mevaric <vladislav.khripunov@gmail.com>
2017-10-25 00:11:31 +0700

    Snake create window

--- SimpleSnake/SimpleSnake.cpp
+++ SimpleSnake/SimpleSnake.cpp
@@ -6,59 +6,19 @@
 // NOT WORK YET
 #include "stdafx.h"
 #include <iostream>
-using namespace std;
-bool gameOver;
-const int width = 20;
-const int height = 80;
-int x, y, fruitX, fruitY, score;
-enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
-eDirection dir;
-void Setup()
-{
-	gameOver = false;
-	dir = STOP;
-	x = width / 2;
-	y = height / 2;
-	fruitX = rand() % width;
-	fruitY = rand() % height;
-	score = 0;
-}
-void Draw()
-{
-	system("cls");
-	for (int i = 0; i < width; i++)
-		cout << "#";
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++)
-		{
-			if (j == 0)
-				cout << "#";
-			
-				cout << " ";
 
-			if (j == width - 1)
-				cout << "#";
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < height; i++)
-		cout << "#";
-	cout << endl;
-}
-void Input()
-{}
-void Logic()
-{}
 int main()
 {
-	Setup();
-	while (!gameOver)
-	{
-		Draw();
-		Input();
-		Logic();
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window * window = SDL_CreateWindow("snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Renderer * render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	bool runing = true;
+
+	while (runing) {
+	
 	}
     return 0;
 }
