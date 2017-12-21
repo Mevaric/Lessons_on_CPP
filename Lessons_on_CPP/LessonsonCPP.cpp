8a5eb05848d0d2f7ca35c676cb2b58b887e3b006
Vladislav Khripunov <vlad_xp_999@mail.ru>
2017-12-21 23:50:44 +0700

    add Simple FTP for upload file (log to ftp server)

--- Lessons_on_CPP/LessonsonCPP.cpp
+++ Lessons_on_CPP/LessonsonCPP.cpp
@@ -2,9 +2,28 @@
 //
 
 #include "stdafx.h"
+#include <vector>
 
-int main()
-{
-	return 0;
+using namespace std;
+
+int simpleArraySum(int n, vector <int> ar) {
+	// Complete this function
+	int sum = 0;
+	vector<int>::iterator par;
+	for (par = ar.begin(); par <= ar.end(); par++)
+		sum += *par;
+	
+	return sum;
 }
 
+int main() {
+	int n;
+	cin >> n;
+	vector<int> ar(n);
+	for (int ar_i = 0; ar_i < n; ar_i++) {
+		cin >> ar[ar_i];
+	}
+	int result = simpleArraySum(n, ar);
+	cout << result << endl;
+	return 0;
+}
